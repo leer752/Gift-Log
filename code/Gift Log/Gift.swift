42e9dfed19d3aaa1f84adad899237f47f2546a86
LeeR <52798154+leer752@users.noreply.github.com>
2019-08-16 14:28:33 -0500

    Folder movement & project upload

---
+++ code/Gift Log/Gift.swift
@@ -0,0 +1,57 @@
+//
+//  Gift.swift
+//  Gift Log
+//
+//  Created by Lee Rhodes on 8/16/19.
+//  Copyright © 2019 Lee Rhodes. All rights reserved.
+//
+
+import UIKit
+
+class Gift {
+    // MARK: Properties
+    
+    var photo: UIImage?
+    var name: String
+    var store: String
+    var address: String?
+    var cityState: String?
+    var url: String?
+    var price: Float
+    var date: String?
+    var itemCode: String?
+    var priority: Int
+    
+    // MARK: Initialization
+    
+    init?(photo: UIImage?, name: String, store: String, address: String?, cityState: String?, url: String?, price: Float, date: String?, itemCode: String?, priority: Int) {
+        
+        guard !name.isEmpty else {
+            return nil
+        }
+        
+        guard !store.isEmpty else {
+            return nil
+        }
+        
+        guard (priority >= 0) && (priority <= 3) else {
+            return nil
+        }
+        
+        guard (price >= 0) else {
+            return nil
+        }
+        
+        self.photo = photo
+        self.name = name
+        self.store = store
+        self.address = address
+        self.cityState = cityState
+        self.url = url
+        self.price = price
+        self.date = date
+        self.itemCode = itemCode
+        self.priority = priority
+    }
+
+}
---
+++ code/Gift LogTests/Gift_LogTests.swift
@@ -0,0 +1,38 @@
+//
+//  Gift_LogTests.swift
+//  Gift LogTests
+//
+//  Created by Lee Rhodes on 8/15/19.
+//  Copyright © 2019 Lee Rhodes. All rights reserved.
+//
+
+import XCTest
+@testable import Gift_Log
+
+class Gift_LogTests: XCTestCase {
+    
+    // Gift Class Tests
+    
+    func testGiftInitalizationSucceeds() {
+        let zeroPriorityGift = Gift.init(photo: nil, name: "Zero", store: "Test", address: nil, cityState: nil, url: nil, price: 0.20, date: nil, itemCode: nil, priority: 0)
+        XCTAssertNotNil(zeroPriorityGift)
+        
+        let highPriorityGift = Gift.init(photo: nil, name: "High", store: "Test", address: nil, cityState: nil, url: nil, price: 30.50, date: nil, itemCode: nil, priority: 3)
+        XCTAssertNotNil(highPriorityGift)
+    }
+    
+    func testGiftInitializationFails() {
+        let negativePriorityGift = Gift.init(photo: nil, name: "Negative", store: "Test", address: nil, cityState: nil, url: nil, price: 20.00, date: nil, itemCode: nil, priority: -6)
+        XCTAssertNil(negativePriorityGift)
+        
+        let noNameGift = Gift.init(photo: nil, name: "", store: "Test", address: nil, cityState: nil, url: nil, price: 0.01, date: nil, itemCode: nil, priority: 0)
+        XCTAssertNil(noNameGift)
+        
+        let negativePriceGift = Gift.init(photo: nil, name: "Free", store: "Test", address: nil, cityState: nil, url: nil, price: -60, date: nil, itemCode: nil, priority: 0)
+        XCTAssertNil(negativePriceGift)
+        
+        let excessPriorityGift = Gift.init(photo: nil, name: "Max", store: "Test", address: nil, cityState: nil, url: nil, price: 40.00, date: nil, itemCode: nil, priority: 6)
+    }
+    
+}
+
